f62448d5c2269984d6e0b53b65c2ed571de9b0d8
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-01-19 17:10:37 +0530

    Create Swap2NumbersWithout3rdVariable.java
    How do you swap two numbers without using a third variable in Java.

---
+++ src/FromFirst/Swap2NumbersWithout3rdVariable.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int a = 10;
+        int b = 20;
+        
+        a = a + b; // a = 10 + 20 = 30
+        b = a - b; // b = 30 - 20 = 10
+        a = a - b; // a = 30 - 10 = 20
+        
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+
+    }
+}
