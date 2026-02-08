952b71d469ea4d0fa84c0db7a5f98f5cd0cee283
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-02-08 16:53:09 +0530

    Solution for DeadLock is to maintain same sequence of objects.

---
+++ src/FromFirst/Threads/DeadLockSolution.java
@@ -0,0 +1,62 @@
+class BookTicket extends Thread {
+    Object train, comp;
+    
+    BookTicket(Object train, Object comp) {
+        this.train = train;
+        this.comp = comp;
+    }
+    
+    public void run() {
+        synchronized(train) {
+            System.out.println("BookTicket locked on train");
+            try {
+               Thread.sleep(150); 
+            } catch(InterruptedException e) { }
+             System.out.println("BookTicket now waiting to lock on compartment");
+             synchronized(comp) {
+                 System.out.println("BookTicket locked on compartment");
+             }
+        }
+    }
+}
+
+class CancelTicket extends Thread {
+    Object train, comp;
+    
+    CancelTicket(Object train, Object comp) {
+        this.train = train;
+        this.comp = comp;
+    }
+    
+    public void run() {
+        synchronized(train) {
+            System.out.println("CancelTicket locked on train");
+            try {
+               Thread.sleep(200); 
+            } catch(InterruptedException e) { }
+             System.out.println("CancelTicket now waiting to lock on compartment");
+             synchronized(comp) {
+                 System.out.println("CancelTicket locked on compartment");
+             }
+        }
+    }
+}
+
+
+
+class Main {
+    public static void main(String[] args) {
+        Object train = new Object();
+        Object compartment = new Object();
+        
+        BookTicket obj1 = new BookTicket(train, compartment);
+        CancelTicket obj2 = new CancelTicket(train, compartment);
+        
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        
+        t1.start();
+        t2.start();
+        
+    }
+}
