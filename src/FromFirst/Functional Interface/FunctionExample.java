da1858de5df933646b652395778e5a7409f5e6d7
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-02-16 20:15:02 +0530

    Example of Function Functional Interface.

---
+++ src/FromFirst/Functional Interface/FunctionExample.java
@@ -0,0 +1,8 @@
+import java.util.function.Function;
+
+class FunctionExample {
+    public static void main(String[] args) {
+        Function<Integer, Double> half = a -> a / 2.0;
+        System.out.println(half.apply(10));
+    }
+}
