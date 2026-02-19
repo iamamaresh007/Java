fccbfbf3bc4318685b6238adf5503a6422517746
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-02-19 20:43:17 +0530

    Code to Partition Employees Age > 30.

---
+++ src/FromFirst/Streams API/Employees/PartitionEmpAge.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Employee {
+    private int id; 
+    private String name;
+    private String dept;
+    private double salary;
+    private int age;
+    
+    Employee(int id, String name, String dept, double salary, int age) {
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        this.age = age;
+    }
+    public int getId() {
+    return id;
+    }
+
+    public String getName() {
+    return name;
+    }
+
+    public String getDept() {
+    return dept;
+    }
+
+    public double getSalary() {
+    return salary;
+    }
+
+    public int getAge() {
+    return age;
+    }
+    
+    
+ @Override
+    public String toString() {
+        return String.format(
+            "Employee{id=%d, name='%s', dept='%s', salary=%.2f, age=%d}",
+            id, name, dept, salary, age
+        );
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        List<Employee> employees = List.of(
+            new Employee(1, "Amaresh", "IT", 60000, 28), 
+            new Employee(2, "Dhoni", "Sports", 70000, 46), 
+            new Employee(3, "Yash", "Entertainment", 30000, 40), 
+            new Employee(4, "Ganesh", "Entertainment", 40000, 32), 
+            new Employee(5, "Karna", "IT", 100000, 26));
+            
+            Map<Boolean, List<Employee>> agePartition = employees.stream().collect(Collectors.partitioningBy(e -> e.getAge() > 30));
+            
+            System.out.println("After age > 30 partition " + agePartition);
+            System.out.println("------------------------------------------------------------------------------------");
+            
+            agePartition.forEach((k, v) -> {
+                if(k) System.out.println("Employee age > 30 are " + v);
+                 else System.out.println("Employee age < 30 are " + v);
+            }
+        );
+    }
+}
