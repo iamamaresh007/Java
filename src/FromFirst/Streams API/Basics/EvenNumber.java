e8ae57958c478ea15a3b0864cd940d186f3b6023
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-07-19 22:02:30 +0530

    Question 1: Filter Even Numbers using filter().

--- src/FromFirst/Streams API/Basics/EvenNumber.java
+++ src/FromFirst/Streams API/Basics/EvenNumber.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(2, 5, 8, 9, 12, 15, 18);
-        List<Integer> result = numbers.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
-        System.out.println(result);
+        List<Integer> output = numbers.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
+        System.out.println(output);
     }
 }
