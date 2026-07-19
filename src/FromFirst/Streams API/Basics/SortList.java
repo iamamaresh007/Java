fc2306a86f64c5ac1ce0edc3e1f533541b803492
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-07-19 22:20:36 +0530

    Question 4 : Sort list in both ASC & DESC order using sorted() and sorted(Comparator.reverseOrder()).

--- src/FromFirst/Streams API/Basics/SortList.java
+++ src/FromFirst/Streams API/Basics/SortList.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(8, 2, 5, 1, 9, 4);
+        List<Integer> numbers = Arrays.asList(8, 2, 6, 1, 9, 4);
         List<Integer> ascending = numbers.stream().sorted().collect(Collectors.toList());
         List<Integer> descending = numbers.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         System.out.println(ascending);
