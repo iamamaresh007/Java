81041b0c740118c79c892984c982b6395c6fe95b
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-07-20 20:21:04 +0530

    Question 13 : Convert List to Map using toMap(Function.identity()).

---
+++ src/FromFirst/Streams API/Basics/ConvertListToMap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.function.Function;
+
+class Main {
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("Apple", "Banana", "Mango");
+        Map<String, Integer> result = fruits.stream().collect(Collectors.toMap(Function.identity(), String::length));
+        //In Java Streams, Function.identity() is a static utility method that returns a function that always returns its exact input argument.
+        //static <T> Function<T, T> identity() { return t -> t; }
+        System.out.println(result);
+    }
+}
