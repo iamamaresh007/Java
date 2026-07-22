38e7eb2aefe19e146b1b3e782039e2c3cad4c9fa
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-07-22 23:48:13 +0530

    Code to find 1st repeating number.

--- src/FromFirst/Streams API/InterviewQuestions/FindFirstRepeatingNumber.java
+++ src/FromFirst/Streams API/InterviewQuestions/FindFirstRepeatingNumber.java
@@ -6,10 +6,10 @@ class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(2, 4, 5, 4, 7, 9, 3, 1, 3);
         Map<Integer, Long> frequency = numbers.stream().collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
-        Integer firstOccurance = frequency.entrySet().stream().filter(e -> e.getValue() > 1).map(Map.Entry::getKey).findFirst().orElse(-1);
+        Integer firstRepeatingNumber = frequency.entrySet().stream().filter(e -> e.getValue() > 1).map(Map.Entry::getKey).findFirst().orElse(-1);
         
         System.out.println(frequency);
-        System.out.println(firstOccurance);
+        System.out.println(firstRepeatingNumber);
         
     }
 }
