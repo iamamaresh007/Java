ad2aedf8fa1fdacb95b9daaae7c8f958a3ea437e
Amaresh Basanagouda Patil <61202675+iamamaresh007@users.noreply.github.com>
2026-07-23 17:19:20 +0530

    Program to find Highest Salary By Department.

---
+++ src/FromFirst/Streams API/Basics/Employee2.O/HighestSalaryByDept.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Employee {
+    private int id;
+    private String name;
+    private double salary;
+    private String department;
+
+    // Constructor
+    public Employee(int id, String name, double salary, String department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    
+    public String getDepartment() {
+        return department;
+    }
+    
+    @Override
+public String toString() {
+    return "Employee{id =" + id +
+           ", name ='" + name + '\'' +
+           ", salary = " + salary +
+           ", department = " + department +
+           '}';
+    
+}
+}
+
+class Main {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<Employee>();
+    employees.add(new Employee(1, "Amaresh", 12000, "IT"));
+    employees.add(new Employee(2, "Ganesh", 8000, "Entertainment"));
+    employees.add(new Employee(3, "Dhoni", 7000, "Sports"));
+    employees.add(new Employee(4, "Yash", 8000, "Entertainment"));
+    employees.add(new Employee(5, "Virat", 18000, "Sports"));
+    employees.add(new Employee(6, "Tom", 10000, "Entertainment"));
+    
+    Map<String, Optional<Employee>> highestSalaryByDept = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.maxBy(Comparator.comparing(Employee::getSalary))));
+    System.out.println(highestSalaryByDept);
+    }
+}
